e8d141eb4397c12e6f9a9a41b5fa619c23ce96ba
agung widhiatmojo <47983775+agungwa@users.noreply.github.com>
2020-03-24 22:13:56 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,15 +7,8 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-@app.route("/sms", methods=['POST'])
+@app.route("/wa", methods=['POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    # msg = request.form.get('Body')
-
-    # Create reply
-    # resp = MessagingResponse()
-    # resp.message("Kamu Bilang: {}".format(msg))
 
     incoming_msg = request.values.get('Body', '').lower()
     resp = MessagingResponse()
@@ -23,16 +16,6 @@ def sms_reply():
     responded = False
     op = ['Selamat datang di toko Agung widhiatmojo', 'order: Cara Order Barang', 'list: List Produk Barang', 'askme: Tanya Status Pemilik Toko','','Ketik order , list , atau askme  kami bisa menjawab pertanyaan mu. ']
     listp = ['1. Makanan', '2. Minuman', '3. Baju', '4. dll', 'silakan kunjungi agungw.net untuk barang yang lain ']
-    if 'quote' in incoming_msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-        responded = True
     if 'order' in incoming_msg:
         msg.body('tinggal order aja.')
         responded = True
